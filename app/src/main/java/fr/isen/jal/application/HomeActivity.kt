5384cd97f1c1cbfde4d6b4cf892af49d01277254
auriane0405 <auriane.coffinet@isen.yncrea.fr>
2022-02-22 16:18:30 +0100

    création page

--- app/src/main/java/fr/isen/jal/application/HomeActivity.kt
+++ app/src/main/java/fr/isen/jal/application/HomeActivity.kt
@@ -2,13 +2,14 @@ package fr.isen.jal.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.jal.application.databinding.ActivityHomeBinding
 
-lateinit var binding: ActivityMainBinding
+lateinit var binding: ActivityHomeBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setContentView(R.layout.fragment_connection)
