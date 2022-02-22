0366bf517fe9ce51017288bedff87e7f363feb53
auriane0405 <auriane.coffinet@isen.yncrea.fr>
2022-02-22 15:59:44 +0100

    création page

---
+++ app/src/main/java/fr/isen/jal/application/CreationFragment.java
@@ -0,0 +1,13 @@
+package fr.isen.jal.application;
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.isen.jal.application.databinding.FragmentRegisterBinding
+
+class CreationFragment : Fragment() {
+    lateinit var binding : FragmentRegisterBinding
+}
--- app/src/main/java/fr/isen/jal/application/HomeActivity.kt
+++ app/src/main/java/fr/isen/jal/application/HomeActivity.kt
@@ -1,67 +1,16 @@
 package fr.isen.jal.application
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.widget.Toast
-import fr.isen.jal.application.databinding.ActivityHomeBinding
 
-class HomeActivity : BaseActivity() {
-    private lateinit var binding: ActivityHomeBinding
+lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        listenClick()
 
-        Log.d("life cycle", "HomeActivity onCreate")
+        setContentView(R.layout.fragment_connection)
     }
-
-    private fun listenClick() {
-        binding.buttonStater.setOnClickListener {
-            showCategory(LunchType.STARTER)
-        }
-        binding.buttonMain.setOnClickListener {
-            showCategory(LunchType.MAIN)
-        }
-        binding.buttonFinish.setOnClickListener {
-            showCategory(LunchType.FINISH)
-        }
-    }
-
-    private fun showCategory(item: LunchType) {
-        val intent = Intent(this, CategoryActivity::class.java)
-        intent.putExtra(HomeActivity.CategoryType, item)
-        startActivity(intent)
-    }
-
-    override fun onDestroy() {
-        Log.d("life cycle", "HomeActivity onDestroy")
-        super.onDestroy()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("life cycle", "HomeActivity onRestart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("life cycle", "HomeActivity onResume")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("life cycle", "HomeActivity onStart")
-    }
-
-    override fun onStop() {
-        Log.d("life cycle", "HomeActivity onStop")
-        super.onStop()
-    }
-
-    companion object {
-        const val CategoryType = "CategoryType"
-    }
+}
