d1a7c65b487ae7322801523e5696d916acd66e67
auriane0405 <auriane.coffinet@isen.yncrea.fr>
2022-02-22 15:33:17 +0100

    création page

--- app/src/main/java/fr/isen/jal/application/LoginFragment.java
+++ app/src/main/java/fr/isen/jal/application/LoginFragment.java
@@ -1,4 +1,48 @@
 package fr.isen.jal.application;
 
-public class LoginFragment {
-}
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.isen.jal.application.R
+import fr.isen.jal.application.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+        lateinit var binding: FragmentLoginBinding
+        var interactor: UserActivityFragmentInteraction? = null
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        }
+
+        override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+        ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+        }
+
+        override fun onAttach(context: Context) {
+        super.onAttach(context)
+        interactor = context as? UserActivityFragmentInteraction
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.noAccountButton.setOnClickListener {
+        interactor?.showRegister()
+        }
+
+        binding.loginButton.setOnClickListener {
+        interactor?.makeRequest(
+        binding.email.text.toString(),
+        binding.password.text.toString(),
+        null,
+        null,
+        true
+        )
+        }
+        }
+        }
