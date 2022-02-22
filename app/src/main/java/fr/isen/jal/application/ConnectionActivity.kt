381f03ca9afd77efeffaa09434862c11149eb217
auriane0405 <auriane.coffinet@isen.yncrea.fr>
2022-02-22 18:26:11 +0100

    création page

--- app/src/main/java/fr/isen/jal/application/ConnectionActivity.kt
+++ app/src/main/java/fr/isen/jal/application/ConnectionActivity.kt
@@ -2,10 +2,23 @@ package fr.isen.jal.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.jal.application.databinding.ActivityConnectionBinding
+
 
 class ConnectionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityConnectionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connection)
+        listenClick()
     }
+
+    private fun listenClick() {
+        binding.buttonConnection.setOnClickListener {
+
+        }
+    }
+
+
 }
+
--- app/src/main/java/fr/isen/jal/application/HomeActivity.kt
+++ app/src/main/java/fr/isen/jal/application/HomeActivity.kt
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import fr.isen.jal.application.databinding.ActivityHomeBinding
 
-lateinit var binding: ActivityHomeBinding
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setContentView(R.layout.fragment_connection)
+
     }
 }
